920431248c8fe32eaf83848b7e1f703fb34c73ad
Zach Neibaur <zneibaur@protonmail.com>
2025-01-11 06:59:37 -0700

    Adding accessory view example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import { PUBLIC_MAP_TOKEN } from '$env/static/public';
   import { onMount } from 'svelte';
   import marker from '$lib/icons/marker.svg'
+  import accessory from '$lib/icons/accessory.svg'
 
   // Global variables to hold the map and mapkit objects
   let map;
@@ -45,12 +46,47 @@
       }
     }
   }
+
+  // Callout delegate
+  const annotationCallout = {
+    calloutLeftAccessoryForAnnotation: () => {
+        const accessoryViewLeft = document.createElement("div");
+        accessoryViewLeft.className = "left-accessory-view";
+
+        const accessoryViewLeftIcon = document.createElement("span");
+        accessoryViewLeftIcon.textContent = "\u{26C5}"; // Sun & Clouds
+        accessoryViewLeft.appendChild(accessoryViewLeftIcon);
+
+        const accessoryViewLeftText = document.createElement("div");
+        accessoryViewLeftText.textContent = "73 \u{00b0}F";
+        accessoryViewLeft.appendChild(accessoryViewLeftText);
+
+        return accessoryViewLeft;
+    },
+  };
+
+  function addAccessoryView() {
+    if (map && mapkitGlobal) {
+      const annotation = new mapkitGlobal.MarkerAnnotation(new mapkitGlobal.Coordinate(37.334883, -122.008977), {
+        title: "Apple Park",
+        subtitle: "Cupertino, CA",
+        color: "#360b5b",
+        selected: true,
+        callout: annotationCallout
+      });
+      map.addAnnotation(annotation);
+    }
+  }
 </script>
 
 <div id="mapContainer">
   <button class="menu-btn" onclick={addAnnotation} style:top="60px">
     <img src={marker} alt="marker" />
     <span>Annotations</span>
+  </button>
+  <button class="menu-btn" onclick={addAccessoryView} style:top="110px">
+    <img src={accessory} alt="accessory" />
+    <span>Accessory Views</span>
   </button>
 </div>
 
