c71e91054b6fb5dd82060f995370688968f06d57
Zach Neibaur <zneibaur@protonmail.com>
2025-01-21 23:46:45 -0700

    Styles and popovers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -67,20 +67,25 @@
 
   function addAccessoryView() {
     if (map && mapkitGlobal) {
-      const annotation = new mapkitGlobal.MarkerAnnotation(new mapkitGlobal.Coordinate(37.334883, -122.008977), {
-        title: "Apple Park",
-        subtitle: "Cupertino, CA",
-        color: "#360b5b",
-        selected: true,
-        callout: annotationCallout
-      });
-      map.addAnnotation(annotation);
+      const activeAnnotation = map.selectedAnnotation;
+      if (activeAnnotation) {
+        map.removeAnnotation(activeAnnotation);
+      } else {
+        const annotation = new mapkitGlobal.MarkerAnnotation(new mapkitGlobal.Coordinate(37.334883, -122.008977), {
+          title: "Apple Park",
+          subtitle: "Cupertino, CA",
+          color: "#360b5b",
+          selected: true,
+          callout: annotationCallout
+        });
+        map.addAnnotation(annotation);
+      }
     }
   }
 </script>
 
 <div id="mapContainer">
-  <button class="menu-btn" onclick={addAnnotation} style:top="60px">
+  <button class="menu-btn" popovertarget="custom-popover" popovertargetaction="toggle" style:top="60px">
     <img src={marker} alt="marker" />
     <span>Annotations</span>
   </button>
@@ -88,12 +93,27 @@
     <img src={accessory} alt="accessory" />
     <span>Accessory Views</span>
   </button>
+  <div popover id="custom-popover" class="popover">
+    <p>A lot of code isn't needed to add a custom annotation. See the sample code <a href="https://gist.github.com/zneib/3034e5fe15fb8620a052cf95e60468f5" target="_blank">here</a></p>
+    <button class="toggle" onclick={addAnnotation}>Toggle Custom Annotation</button>
+  </div>
 </div>
 
 <style>
   :global(root, body, *) {
     box-sizing: border-box;
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    --gray-one: #1f2933;
+    --gray-two: #323F4B;
+    --gray-three: #3E4C59;
+    --gray-four: #52606D;
+    --gray-five: #616E7C;
+    --gray-six: #7b8794;
+    --gray-seven: #9aa5b1;
+    --gray-eight: #cbd2d9;
+    --gray-nine: #E4E7EB;
+    --gray-ten: #F5F7FA;
+    --blue-ten: #486581;
   }
   :global(body) {
     margin: 0px;
@@ -125,10 +145,26 @@
     background-color: rgba(0, 0, 0, 0.3);
   }
 
+  .toggle {
+    background-color: var(--blue-ten);
+    color: white;
+    border: none;
+    border-radius: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+    width: fit-content;
+  }
+
   .popover:popover-open {
     position: absolute;
     top: 50px;
     left: 200px;
-    margin: 0
+    margin: 0;
+    border-radius: 8px;
+    padding: 10px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: rgba(255,255,255,0.75);
   }
 </style>
