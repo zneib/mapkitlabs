bbc2efdfb08693ba51ac15e5fbfd43396b320e25
Zach Neibaur <zach@zbar.dev>
2025-09-05 22:50:30 -0600

    Adding some more options to annotations

--- src/lib/MarkerAnnotation/MarkerAnnotation.svelte
+++ src/lib/MarkerAnnotation/MarkerAnnotation.svelte
@@ -4,8 +4,9 @@
 
   let annotationAdded = $state(false);
   let markerAnnotationTitle = $state('');
-  let markerAnnotationSubtitle = $state('Cupertino, CA');
+  let markerAnnotationSubtitle = $state('');
   let markerAnnotationColor = $state('30b567');
+  let markerAnnotationSelected = $state(true);
 
   function addAnnotation() {
     if (map && mapkitGlobal) {
@@ -14,7 +15,7 @@
         title: markerAnnotationTitle,
         subtitle: markerAnnotationSubtitle,
         color: markerAnnotationColor,
-        selected: true
+        selected: markerAnnotationSelected
       });
       map.addAnnotation(annotation);
       annotationAdded = true;
@@ -29,14 +30,22 @@
   }
   function resetAnnotationWithNewData() {
     if (map && mapkitGlobal) {
-      map.removeAnnotation(map.selectedAnnotation);
+      console.log(map)
+      map.removeAnnotations(map._impl._annotationsController._items); // Remove all annotations
+      // map.removeAnnotation(map.selectedAnnotation); // Only remove the selected annotation, which won't work if the annotation isn't selected
       const annotation = new mapkitGlobal.MarkerAnnotation(new mapkitGlobal.Coordinate(37.334883, -122.008977), {
         title: markerAnnotationTitle,
         subtitle: markerAnnotationSubtitle,
         color: markerAnnotationColor,
-        selected: true
+        selected: markerAnnotationSelected
       });
       map.addAnnotation(annotation);
+    }
+  }
+  function removeAllAnnotations() {
+    if (map && mapkitGlobal) {
+      map.removeAnnotations(map._impl._annotationsController._items);
+      annotationAdded = false;
     }
   }
 </script>
@@ -47,7 +56,6 @@
 </button>
 <div popover id="custom-popover" class="popover">
   <p>See the sample code <a href="https://gist.github.com/zneib/3034e5fe15fb8620a052cf95e60468f5" target="_blank">here</a></p>
-  <button class="toggle" onclick={annotationAdded ? removeCustomAnnotation : addAnnotation}>Toggle Custom Annotation</button>
   <div class="annotation-input">
     <label for="markerAnnotationTitle">Title</label>
     <input bind:value={markerAnnotationTitle} class="annotation-input" type="text" name="markerAnnotationTitle" id="markerAnnotationTitle" placeholder="Enter a title for the marker annotation" onblur={resetAnnotationWithNewData} />
@@ -58,7 +66,15 @@
   </div>
   <div class="annotation-input">
     <label for="markerAnnotationColor">Color</label>
-    <input bind:value={markerAnnotationColor} class="annotation-input" type="color" name="markerAnnotationColor" id="markerAnnotationColor" placeholder="Enter a color for the marker annotation" onblur={addAnnotation} />
+    <input bind:value={markerAnnotationColor} class="annotation-input" type="color" name="markerAnnotationColor" id="markerAnnotationColor" placeholder="Enter a color for the marker annotation" onblur={resetAnnotationWithNewData} />
+  </div>
+  <div class="annotation-input">
+    <label for="markerAnnotationSelected">Selected</label>
+    <input bind:checked={markerAnnotationSelected} class="annotation-input" type="checkbox" name="markerAnnotationSelected" id="markerAnnotationSelected" onchange={resetAnnotationWithNewData} />
+  </div>
+  <div class="button-row">
+    <button class="toggle" onclick={removeAllAnnotations}>Remove All Annotations</button>
+    <button class="toggle" onclick={annotationAdded ? removeCustomAnnotation : addAnnotation}>Toggle Custom Annotation</button>
   </div>
 </div>
 
@@ -82,15 +98,22 @@
   ::backdrop {
     background-color: rgba(0, 0, 0, 0.3);
   }
-
+  .button-row {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    margin-top: 10px;
+  }
   .toggle {
-    background-color: var(--blue-ten);
-    color: white;
-    border: none;
+    background-color: transparent;
+    color: var(--gray-one);
+    border: 1px solid var(--gray-one);
     border-radius: 5px;
     padding: 10px 20px;
     cursor: pointer;
     width: fit-content;
+    margin-top: 10px;
+    text-transform: uppercase;
   }
 
   .popover:popover-open {
