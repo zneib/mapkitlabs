27f778b8c344026834b3d5eb3084437388a82e64
Zach Neibaur <zneibaur@protonmail.com>
2025-09-13 08:04:09 -0600

    Wiring up the close button

--- src/lib/MarkerAnnotation/MarkerAnnotation.svelte
+++ src/lib/MarkerAnnotation/MarkerAnnotation.svelte
@@ -54,6 +54,10 @@
     markerAnnotationColor = color;
     resetAnnotationWithNewData();
   }
+  function closePopover() {
+    const popoverId = $state(document.getElementById('custom-popover'));
+    popoverId.hidePopover();
+  }
 </script>
 
 <button class="menu-btn" popovertarget="custom-popover" popovertargetaction="toggle" style:top="60px">
@@ -68,7 +72,7 @@
         <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
       </svg>
     </a>
-    <button class="close-btn">&times;</button>
+    <button class="close-btn" onclick={closePopover}>&times;</button>
   </div>
   <div class="section-wrapper">
     <aside class="option-section">
