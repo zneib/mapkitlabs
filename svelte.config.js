2e6473064ba913d4032317f6d9792bf710337d28
Zach Neibaur <zneibaur@protonmail.com>
2025-09-21 20:33:37 -0600

    Trying out the svelte inspector plugin

--- svelte.config.js
+++ svelte.config.js
@@ -7,6 +7,13 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
+	},
+	vitePlugin: {
+		inspector: {
+			toggleKeyCombo: 'command-alt-x',
+			showToggleButton: 'always',
+			toggleButtonPos: 'bottom-right'
+		}
 	}
 };
 
